6569bcab6a87664be57191f6f09062917812b966
Tsao <Tsao@AppledeMacBook-Pro.local>
2016-07-31 10:13:33 +0800

    fix

--- SCGIFImageView.h
+++ SCGIFImageView.h
@@ -8,23 +8,54 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SCGIFImageFrame : NSObject {
-    
+@interface AnimatedGifFrame : NSObject
+{
+    NSData *data;
+    NSData *header;
+    double delay;
+    int disposalMethod;
+    CGRect area;
 }
-@property (nonatomic) double duration;
-@property (nonatomic, retain) UIImage* image;
+
+@property (nonatomic, copy) NSData *header;
+@property (nonatomic, copy) NSData *data;
+@property (nonatomic) double delay;
+@property (nonatomic) int disposalMethod;
+@property (nonatomic) CGRect area;
 
 @end
 
 @interface SCGIFImageView : UIImageView {
-    NSInteger _currentImageIndex;
+    NSData *GIF_pointer;
+    NSMutableData *GIF_buffer;
+    NSMutableData *GIF_screen;
+    NSMutableData *GIF_global;
+    NSMutableArray *GIF_frames;
+    
+    int GIF_sorted;
+    int GIF_colorS;
+    int GIF_colorC;
+    int GIF_colorF;
+    int animatedGifDelay;
+    
+    int dataPointer;
 }
-@property (nonatomic, retain) NSArray* imageFrameArray;
-@property (nonatomic, retain) NSTimer* timer;
+@property (nonatomic, retain) NSMutableArray *GIF_frames;
 
-//Setting this value to pause or continue animation;
-@property (nonatomic) BOOL animating;
+- (id)initWithGIFFile:(NSString*)gifFilePath;
+- (id)initWithGIFData:(NSData*)gifImageData;
 
-- (void)setData:(NSData*)imageData;
+- (void)loadImageData;
+
++ (NSMutableArray*)getGifFrames:(NSData*)gifImageData;
++ (BOOL)isGifImage:(NSData*)imageData;
+
+- (void) decodeGIF:(NSData *)GIFData;
+- (void) GIFReadExtensions;
+- (void) GIFReadDescriptor;
+- (bool) GIFGetBytes:(int)length;
+- (bool) GIFSkipBytes: (int) length;
+- (NSData*) getFrameAsDataAtIndex:(int)index;
+- (UIImage*) getFrameAsImageAtIndex:(int)index;
 
 @end
